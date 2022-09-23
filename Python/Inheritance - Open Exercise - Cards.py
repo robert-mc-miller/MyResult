0ac6bfcc31d7c2fe0c86175b09929009ea4169db
robert-mc-miller <56699379+robert-mc-miller@users.noreply.github.com>
2022-09-23 14:33:56 +0100

    Update Inheritance - Open Exercise - Cards.py

--- Python/Inheritance - Open Exercise - Cards.py
+++ Python/Inheritance - Open Exercise - Cards.py
@@ -1,3 +1,4 @@
+from hashlib import sha3_224
 import random
 
 class Card():
@@ -52,21 +53,74 @@ class Stack():
 
 class Deck(Stack):
     def __init__(self):
-        self.cards = ["H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","H11","H12","H13","D1","D2","D3","D4","D5","D6","D7","D8","D9","D10","D11","D12","D13","S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11","S12","S13","C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13"]
+        self.cards = [""] * 52
+        suits = ["C", "D", "H", "S"]
+        index =  0
+        for i in range(4):
+            for x in range(1, 14):
+                self.cards[index] = Card(suits[i], x)
+                index += 1
 
     def shuffle(self):
         for i in range(len(self.cards)):
             temp = self.cards[i]
-            randnum = random.randint(0,52)
+            randnum = random.randint(0,51)
             self.cards[i] = self.cards[randnum]
             self.cards[randnum] = temp
 
+    def deal(self, stackList, num):
+        if len(stackList) * num > 52:
+            print("There is not enough cards in a deck to deal that many to each stack.")
+            return
+
+        self.shuffle()
+        temp = self.cards
+        cardsDealt = 0
+        for x in range(len(stackList)):
+            for i in range(num):
+                stackList[x].add(self.cards[cardsDealt]) 
+                cardsDealt += 1
+
+
+class Hand(Stack):
+    def __init__(self, name):
+        self.playerName = name
+        self.cards = []
+
+    def get_player_name(self):
+        return self.playerName
+
+    def add(self, card):
+        self.cards = self.cards + [card]
+        if len(self.cards) > 1:
+            self.sort()
+
+class Hand_OM(Hand):
+    def sort(self):
+        assignments = True
+        
+        while assignments == True:
+            assignments = False
+            
+            for i in range(len(self.cards)-1):
+
+                if float(ord(self.cards[i].get_suit())/100 + (self.cards[i].get_rank())) > float(ord(self.cards[i+1].get_suit())/100 + ((self.cards[i+1].get_rank()))):
+                    
+                    temp = self.cards[i]          
+                    self.cards[i] = self.cards[i+1]
+                    self.cards[i+1] = temp
+                    assignments = True
+
+        return self.cards
 
 c1 = Card("H", 2)
 c2 = Card("D", 3)
 c3 = Card("C", 1)
 c4 = Card("D", 13)
 c5 = Card("D", 1)
+
+
+
 
 s = Stack()
 s.add(c1)
@@ -75,7 +129,37 @@ s.add(c3)
 s.add(c4)
 s.add(c5)
 
+d = Deck()
 
-print(s.get_cards())
-s.sort()
-print(s.get_cards())
+
+s1 = Stack()
+s2 = Stack()
+s3 = Stack()
+s4 = Stack()
+s5 = Stack()
+stackList = [s1, s2, s3, s4, s5]
+
+d.deal(stackList, 10)
+
+h = Hand("Jack")
+h.add(c1)
+h.add(c2)
+h.add(c3)
+h.add(c4)
+
+print(h.get_cards())
+print(h.get_player_name())
+
+h_om  = Hand_OM("James")
+h_om.add(c4)
+h_om.add(c1)
+h_om.add(c2)
+h_om.add(c3)
+h_om.add(c5)
+h_om.add(Card("S", 1))
+h_om.add(Card("S", 1))
+h_om.add(Card("S", 2))
+
+print(h_om.get_cards())
+
+ 
